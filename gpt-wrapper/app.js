87de5d8e8ebb8c12a3d0d092f0e0115d1a840a5c
Abhishek Mishra <mishra.abhishek199917@gmail.com>
2023-12-03 16:18:55 +0530

    re-visit but abandon mostly

--- gpt-wrapper/app.js
+++ gpt-wrapper/app.js
@@ -1,9 +1,24 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const models = require("langchain/chat_models/openai");
 
-app.get("/", (req, res) => {
+const chatModel = models.ChatOpenAI({ modelName: "gpt-4", temperature: 0.1 });
+
+const app = express();
+
+app.get("/", (_, res) => {
   res.send("Hello World!");
+});
+
+app.post("/api/satisfaction", (req, res) => {
+  const requestData = req.body;
+  console.log("Received POST data:", requestData);
+  res.json({ message: "Client is satisfied" });
+});
+
+app.put("api/grid", (req, res) => {
+  const requestData = req.body;
+  console.log("Received PUT data:", requestData);
+  res.json({ message: "Grid is created" });
 });
 
 app.listen(port, () => {
